6fb857a3efcbc574ff825694b2e57eb0315fbccb
guoxbin <guoxbin@gmail.com>
2019-12-25 00:52:30 +0800

    Initialize

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
 # yeebranch
-Official implementation of the YeeCo Branch Chain (Layer 2)
+
+[![standard-readme compliant](https://img.shields.io/badge/readme%20style-standard-brightgreen.svg?style=flat-square)](https://github.com/RichardLitt/standard-readme)
+
+> Official implementation of the YeeCo Branch Chain (Layer 2)
+
+YeeCo is a permissionless, secure, high performance and scalable public blockchain platform powered by full sharding technology on PoW consensus.
+
+## Table of Contents
+
+- [Description](#description)
+- [Install](#install)
+- [Usage](#usage)
+- [Roadmap](#roadmap)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Description
+
+Yeebranch is designed as layer 2 of YeeCo.
+
+| Layer   | Name            |  Consensus   |  Transaction types   | 
+| --------| --------------- | ------------ |--------------| 
+| 1       | [Yeeroot chain](https://github.com/yeeco/yeeroot)   |  <ul><li>POW for block generation </li><li>CRFG for block finalization</li></ul> |  <ul><li> Native coin tx </li><li> Token tx </li><li> Meta tx of branch chains </li></ul>  |
+| 2       | [Yeebranch chain](https://github.com/yeeco/yeebranch) |  Optional <br> <ul><li>DPOS</li><li>POA</li></ul> | <ul><li> Basic tx </li><li> Smart contract tx </li>
+
+## Install
+
+### Requirements
+1. Rust
+    ```sh
+    curl https://sh.rustup.rs -sSf | sh
+    ```
+1. Openssl
+1. Rust nightly
+    ```sh
+    rustup toolchain add nightly
+    ```
+1. rust nightly wasm
+    ```sh
+    rustup target add wasm32-unknown-unknown
+    rustup target add wasm32-unknown-unknown --toolchain nightly
+    ```
+1. wasm-gc
+    ```sh
+    cargo install wasm-gc
+    ```
+1. Rust components: clippy rls docs src rustfmt
+    ```sh
+    rustup component list # list all the components installed
+    rustup component add <name> # install component
+    ```
+
+### Building
+```sh
+$ cd <project_base_dir>/runtime/wasm
+$ sh build.sh
+$ cd <project_base_dir>
+$ cargo build
+```
+
+## Usage
+
+### Start
+
+Start the node
+    ```sh
+    $ ./yee-branch --dev --alice
+    ```
+
+## Contributing
+
+Feel free to dive in! [Open an issue](./issues/new).
+
+### Contributors
+
+
+## License
+
+[GPL](LICENSE)
---
+++ src/cli.rs
@@ -0,0 +1,134 @@
+use crate::service;
+use futures::{future, Future, sync::oneshot};
+use std::cell::RefCell;
+use tokio::runtime::Runtime;
+pub use substrate_cli::{VersionInfo, IntoExit, error};
+use substrate_cli::{informant, parse_and_execute, NoCustom, TriggerExit};
+use substrate_service::{ServiceFactory, Roles as ServiceRoles, Arc};
+use crate::chain_spec;
+use std::ops::Deref;
+use log::info;
+use parking_lot::Mutex;
+use futures::sync::oneshot::Sender;
+use signal_hook::{iterator::Signals, SIGUSR1, SIGINT, SIGTERM};
+use std::thread;
+use std::fmt::Debug;
+
+/// Parse command line arguments into service configuration.
+pub fn run<I, T, E>(args: I, exit: E, version: VersionInfo) -> error::Result<()> where
+	I: IntoIterator<Item = T>,
+	T: Into<std::ffi::OsString> + Clone,
+	E: IntoExit,
+{
+	parse_and_execute::<service::Factory, NoCustom, NoCustom, _, _, _, _, _>(
+		load_spec, &version, "substrate-node", args, exit,
+	 	|exit, _custom_args, config| {
+			info!("{}", version.name);
+			info!("  version {}", config.full_version());
+			info!("  by {}, 2017, 2018", version.author);
+			info!("Chain specification: {}", config.chain_spec.name());
+			info!("Node name: {}", config.name);
+			info!("Roles: {:?}", config.roles);
+			let runtime = Runtime::new().map_err(|e| format!("{:?}", e))?;
+			let executor = runtime.executor();
+			match config.roles {
+				ServiceRoles::LIGHT => run_until_exit(
+					runtime,
+				 	service::Factory::new_light(config, executor).map_err(|e| format!("{:?}", e))?,
+					exit
+				),
+				_ => run_until_exit(
+					runtime,
+					service::Factory::new_full(config, executor).map_err(|e| format!("{:?}", e))?,
+					exit
+				),
+			}.map_err(|e| format!("{:?}", e))
+		}
+	).map_err(Into::into).map(|_| ())
+}
+
+fn load_spec(id: &str) -> Result<Option<chain_spec::ChainSpec>, String> {
+	Ok(match chain_spec::Alternative::from(id) {
+		Some(spec) => Some(spec.load()?),
+		None => None,
+	})
+}
+
+fn run_until_exit<T, C, E>(
+	mut runtime: Runtime,
+	service: T,
+	e: E,
+) -> error::Result<()>
+	where
+		T: Deref<Target=substrate_service::Service<C>>,
+		C: substrate_service::Components,
+		E: IntoExit,
+{
+	let (exit_send, exit) = exit_future::signal();
+
+	let executor = runtime.executor();
+	informant::start(&service, exit.clone(), executor.clone());
+
+	let _ = runtime.block_on(e.into_exit().0);
+	exit_send.fire();
+
+	// we eagerly drop the service so that the internal exit future is fired,
+	// but we need to keep holding a reference to the global telemetry guard
+	let _telemetry = service.telemetry();
+	drop(service);
+	Ok(())
+}
+
+// handles ctrl-c
+pub struct Exit;
+impl IntoExit for Exit {
+	type TriggerExit = CliTriggerExit<CliSignal>;
+	type Exit = future::MapErr<oneshot::Receiver<<Self::TriggerExit as TriggerExit>::Signal>, fn(oneshot::Canceled) -> ()>;
+	fn into_exit(self) -> (Self::Exit, Self::TriggerExit) {
+		// can't use signal directly here because CtrlC takes only `Fn`.
+		let (exit_send, exit) = oneshot::channel();
+
+		let exit_send_cell = Arc::new(Mutex::new(Some(exit_send)));
+		let exit_send_cell_clone = exit_send_cell.clone();
+
+		let signals = Signals::new(&[SIGUSR1, SIGINT, SIGTERM]).unwrap();
+
+		thread::spawn(move || {
+			for sig in signals.forever() {
+				info!("Received signal {:?}", sig);
+
+				let signal = match sig{
+					SIGUSR1 => CliSignal::Restart,
+					_ => CliSignal::Stop,
+				};
+
+				if let Some(sender) = exit_send_cell.lock().take() {
+					sender.send(signal).expect("Error sending exit signal");
+				}
+			}
+		});
+
+		(exit.map_err(drop), CliTriggerExit{sender: exit_send_cell_clone})
+	}
+}
+
+#[derive(Debug)]
+pub enum CliSignal{
+	Stop,
+	Restart,
+}
+
+pub struct CliTriggerExit<Signal>{
+	sender: Arc<Mutex<Option<Sender<Signal>>>>,
+}
+
+impl<Signal> TriggerExit for CliTriggerExit<Signal>
+	where Signal: Send + Debug + 'static {
+	type Signal = Signal;
+	fn trigger_exit(&self, signal: Self::Signal){
+
+		if let Some(sender) = self.sender.lock().take() {
+			sender.send(signal).expect("Error sending exit signal");
+		}
+	}
+}
